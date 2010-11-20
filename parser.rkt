b5476fc7553a801d22649d154b44103747be9729
Daniel King <danking@ccs.neu.edu>
2010-11-20 15:21:40 -0800

    Added *s methods to parse lists of data and updated extensions to .rkt

--- parser.rkt
+++ parser.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(require "tea-data.scm")
+(require "tea-data.rkt")
 (provide parse-tea-defexp)
 
 (define (parse-tea-defexp defexp)
@@ -11,11 +11,14 @@
   (match def
     [(list 'define (list name ids ...) bodies ..1)
      (tea-pdefine (parse-tea-id name)
-                           (map parse-tea-id ids)
-                           (map parse-tea-exp bodies))]
+                  (parse-tea-ids ids)
+                  (parse-tea-exps bodies))]
     [(list 'define id body) (tea-define (parse-tea-id id)
-                                             (parse-tea-exp body))]
+                                        (parse-tea-exp body))]
     [_ (error 'lexer "expected ~s to be a definition")]))
+
+(define (parse-tea-exps exps)
+  (map parse-tea-exp exps))
 
 (define (parse-tea-exp exp)
   (match exp
@@ -23,24 +26,34 @@
     [(? number? _) (tea-number exp)]
     [(? symbol? _) (tea-id exp)]
     [(list 'quote (? symbol? s)) (tea-symbol s)]
-    [(list 'quote ls) (tea-list (map parse-tea-data ls))]
+    [(list 'quote ls) (tea-list (parse-tea-data ls))]
     [(list 'lambda (list ids ...) bodies ..1)
-     (tea-lambda (map parse-tea-id ids)
-                      (map parse-tea-exp bodies))]
+     (tea-lambda (parse-tea-ids ids)
+                 (parse-tea-exps bodies))]
+    [(list let (list (ids vals) ...) body ...)
+     (tea-let (parse-tea-ids ids)
+              (parse-tea-exps vals)
+              (parse-tea-exps body))]
     [(list head tail ...)
      (tea-apply  (parse-tea-exp head)
-                      (map parse-tea-exp tail))]
+                 (parse-tea-exps tail))]
     [_ (error 'lexer "expected ~s to be an expression" exp)]))
+
+(define (parse-tea-ids ids)
+  (map parse-tea-id ids))
 
 (define (parse-tea-id id)
   (if (symbol? id) (tea-id id)
       (error 'lexer "expected ~s to be an identifier" id)))
 
 (define (parse-tea-data data)
-  (match data
-    [(? string? _) (tea-string data)]
-    [(? number? _) (tea-number data)]
-    [(? symbol? _) (tea-symbol data)]
+  (map parse-tea-datum data))
+
+(define (parse-tea-datum datum)
+  (match datum
+    [(? string? _) (tea-string datum)]
+    [(? number? _) (tea-number datum)]
+    [(? symbol? _) (tea-symbol datum)]
     [(list data ...)
-     (tea-list (map parse-tea-data data))]
-    [_ (error 'lexer "expected ~s to be a datum" exp)]))
+     (tea-list (parse-tea-data data))]
+    [_ (error 'lexer "expected ~s to be a datum" datum)]))
