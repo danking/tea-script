aa54a9e62c2eb6820547cec14d09c6997668aedd
Daniel King <danking@ccs.neu.edu>
2010-11-19 23:01:20 -0800

    Added js-to-text test cases for the new JOp structures

--- tests/js-to-text.scm
+++ tests/js-to-text.scm
@@ -5,6 +5,17 @@
 (define sa string-append)
 (define jt jstatement->text)
 
+(define-syntax check-binary-op
+  (syntax-rules ()
+    [(_ op v1 v2)
+     (let [(wrap (lambda (x)
+                   (cond [(string? x) (jstring x)]
+                         [(number? x) (jnumber x)]
+                         [else (error 'wrap "~a is of unknown type" x)])))]
+       (check-equal? (jt (jprimop op (list (wrap v1)
+                                           (wrap v2))))
+                     (format "(~a ~a ~a);\n" v1 op v2)))]))
+
 (define js-to-text-ts
   (test-suite
    "Tests for js-to-text.scm"
@@ -117,6 +128,69 @@
                       "  return x;\n"
                       "});\n")))
    (test-case
+    "Arithmetic Operators"
+    (check-equal? (jt (jprimop '+ (list (jnumber 1) (jnumber 2))))
+                  "(1 + 2);\n")
+    (check-equal? (jt (jprimop '- (list (jnumber 1) (jnumber 5))))
+                  "(1 - 5);\n")
+    (check-equal? (jt (jprimop '+ (list (jnumber 1))))
+                  "(+1);\n")
+    (check-equal? (jt (jprimop '- (list (jnumber 1))))
+                  "(-1);\n")
+    (check-equal? (jt (jprimop '++_ (list (jnumber 3))))
+                  "(++3);\n")
+    (check-equal? (jt (jprimop '_++ (list (jnumber 3))))
+                  "(3++);\n")
+    (check-equal? (jt (jprimop '--_ (list (jnumber 3))))
+                  "(--3);\n")
+    (check-equal? (jt (jprimop '_-- (list (jnumber 3))))
+                  "(3--);\n"))
+   (test-case
+    "Comparison Operators"
+    (check-binary-op '== 3 4)
+    (check-binary-op '!= 3 4)
+    (check-binary-op '=== 3 4)
+    (check-binary-op '!== 3 4)
+    (check-binary-op '> 3 4)
+    (check-binary-op '>= 3 4)
+    (check-binary-op '< 3 4)
+    (check-binary-op '<= 3 4))
+   (test-case
+    "Logical Operators"
+    (check-binary-op '&& 2 5)
+    (check-binary-op '|| 2 5)
+    (check-equal? (jt (jprimop '! (list (jid 'foo))))
+                  "(!foo);\n"))
+   (test-case
+    "JS Bracket Operator"
+    (check-equal? (jt (jbracket (jid 'foo) (jstring "bar")))
+                  "foo[\"bar\"];\n")
+    (check-equal? (jt (jbracket (jnew (jid 'Object) (list))
+                                (jstring " ")))
+                  "new Object()[\" \"];\n"))
+   (test-case
+    "JS Conditional Operator"
+    (check-equal? (jt (jcond (jid 'foo) (jid 'bar) (jid 'baz)))
+                  "(foo ? bar : baz);\n"))
+   (test-case
+    "JS Comma Operator"
+    (check-equal? (jt (jcomma (list (jid 'foo)
+                                    (jid 'bar)
+                                    (jid 'baz))))
+                  "(foo, bar, baz);\n"))
+   (test-case
+    "JS in Operator"
+    (check-equal? (jt (jin (jstring "property") (jid 'myObject)))
+                  "(\"property\" in myObject);\n"))
+   (test-case
+    "JS instanceOf Operator"
+    (check-equal? (jt (jinstof (jid 'foo) (jid 'String)))
+                  "(foo instanceOf String);\n"))
+   (test-case
+    "JS typeof Operator"
+    (check-equal? (jt (jtypeof (jid 'bar)))
+                  "(typeof bar);\n"))
+   (test-case
     "JS Dot Operator"
     (check-equal? (jt (jdot (jid 'foo) (jid 'bar)))
                   "foo.bar;\n")
@@ -132,6 +206,14 @@
                                     (jid 'baz))
                               (list)))
                   "foo.bar().baz();\n"))
+   (test-case
+    "JS New"
+    (check-equal? (jt (jnew (jid 'Object) (list)))
+                  "new Object();\n")
+    (check-equal? (jt (jnew (jid 'Object) (list (jid 'x)
+                                                (jnumber 3)
+                                                (jstring "foo"))))
+                  "new Object(x, 3, \"foo\");\n"))
    (test-case
     "JS Lambda"
     (check-equal? (jt (jlambda (list) (list)))
@@ -151,14 +233,6 @@
                   (sa "(function (x, y) {\n"
                       "  return foo(x, y);\n"
                       "});\n")))
-   (test-case
-    "JS New"
-    (check-equal? (jt (jnew (jid 'Object) (list)))
-                  "new Object();\n")
-    (check-equal? (jt (jnew (jid 'Object) (list (jid 'x)
-                                                (jnumber 3)
-                                                (jstring "foo"))))
-                  "new Object(x, 3, \"foo\");\n"))
    (test-case
     "JS Return"
     (check-equal? (jt (jreturn (jid 'foo)))
