ce5b598a82874a8717e81acfddc77cad4b26d8f0
Daniel King <danking@ccs.neu.edu>
2010-11-20 23:20:54 -0800

    added run-all.rkt which runs all the tests

---
+++ tests/run-all.rkt
@@ -0,0 +1,8 @@
+#lang racket
+(require "parser.rkt")
+(require "tea-to-js.rkt")
+(require "js-expansions.rkt")
+(require "js-to-text.rkt")
+(require "integration.rkt")
+(require "at-runtime.rkt")
+(require "string-utilities.rkt")
