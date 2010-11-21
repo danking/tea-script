5302857e602d0b20b7537cdcefc219d133d090bb
Daniel King <danking@ccs.neu.edu>
2010-11-20 23:19:04 -0800

    updated main.rkt to be aware of the js-expansion stuff

--- main.rkt
+++ main.rkt
@@ -1,8 +1,9 @@
 #lang racket
 (require "parser.rkt"
-         "scheme-to-js.rkt"
+         "tea-to-js.rkt"
+         "js-expansions.rkt"
          "js-to-text.rkt")
 (provide tea->js)
 
 (define (tea->js sexp)
-  (jstatement->text (tea-defexp->js (parse-tea-defexp sexp))))
+  (jstatement->text (expand-js (tea-defexp->js (parse-tea-defexp sexp)))))
