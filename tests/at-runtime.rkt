d74573c7f1fcf6bd9848414b3b4729a7e674a3a0
Daniel King <danking@ccs.neu.edu>
2010-11-20 23:15:37 -0800

    implemented a `run' procedure to execute javascript code in rhino

--- tests/at-runtime.rkt
+++ tests/at-runtime.rkt
@@ -1,7 +1,51 @@
 #lang racket
+(require rackunit
+         "../main.rkt")
+(provide at-runtime-ts)
 
-(define (run x)
-  (error 'run "not implemented!"))
+(define (run sexp)
+  (read-all-lines (first (process (format (string-append "echo '~a' | "
+                                                         "rhino 2>&1 | "
+                                                         "sed 's/js> //'")
+                                          (tea->js sexp))))))
+
+(define (read-all-lines port)
+  (let [(line (read-line port))]
+   (cond [(eof-object? line) '()]
+         [else (cons line (read-all-lines port))])))
+
+(define (lines->output lines)
+  (if (>= (length lines) 3)
+      (third lines)
+      (last lines)))
+
+(define (lines->string lines)
+  (foldl (lambda (line lines)
+           (string-append lines "\n" line))
+         ""
+         lines))
+
+(define-syntax check-runtime
+  (syntax-rules ()
+   [(_ sexp expected) (let [(output (run sexp))]
+                        (check-equal? (lines->output (reverse output))
+                                      expected
+                                      (lines->string output)))]))
 
 ;; symbols
-(check-expect (run '(symbol=? foo-bar_ foo_bar-)) #f) ; depends on gensym
+(define at-runtime-ts
+ (test-suite
+  "runtime tests"
+  (test-case
+   "non-js friendly ids"
+   (check-runtime '(symbol=? foo-bar_ foo_bar-)
+                  "false"))            ; depends on gensym
+  (test-case
+   "let"
+   (check-runtime '(let ([x 3] [y 5])
+                        (+ x y 2))
+                  "10"))))
+
+(require rackunit/text-ui)
+
+(run-tests at-runtime-ts)
