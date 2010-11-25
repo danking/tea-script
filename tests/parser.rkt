13e342de6fb5177362da56f84621c4f8bd3fc9a7
Daniel King <danking@ccs.neu.edu>
2010-11-24 20:45:00 -0800

    fixing indentation in test-suite

--- tests/parser.rkt
+++ tests/parser.rkt
@@ -6,77 +6,77 @@
 (define pt parse-tea-defexp)
 
 (define parser-ts
- (test-suite
-  "tests for the parser"
-  (test-case
-   "define statements"
-   (check-equal? (pt '(define foo 3))
-                 (tea-define (tea-id 'foo) (tea-number 3)))
-   (check-equal? (pt '(define (foo bar baz) (+ bar baz)))
-                 (tea-pdefine (tea-id 'foo)
-                              (list (tea-id 'bar)
-                                    (tea-id 'baz))
-                              (list (tea-apply (tea-id '+)
-                                               (list (tea-id 'bar)
-                                                     (tea-id 'baz)))))))
-  (test-case
-   "self-evals"
-   (check-equal? (pt '3)
-                 (tea-number 3))
-   (check-equal? (pt '"foo")
-                 (tea-string "foo"))
-   (check-equal? (pt 'foo)
-                 (tea-id 'foo))
-   (check-equal? (pt ''foo)
-                 (tea-symbol 'foo)))
-  (test-case
-   "apply"
-   (check-equal? (pt '(+ 1 foo))
-                 (tea-apply (tea-id '+)
-                            (list (tea-number 1)
-                                  (tea-id 'foo)))))
-  (test-case
-   "lambda"
-   (check-equal? (pt '(lambda (x y z) (- x y z)))
-                 (tea-lambda (list (tea-id 'x)
-                                   (tea-id 'y)
-                                   (tea-id 'z))
-                             (list (tea-apply (tea-id '-)
-                                              (list (tea-id 'x)
-                                                    (tea-id 'y)
-                                                    (tea-id 'z)))))))
-  (test-case
-   "if"
-   (check-equal? (pt '(if x (+ 3 6) (y 4)))
-                 (tea-if (tea-id 'x)
-                         (tea-apply (tea-id '+)
-                                    (list (tea-number 3)
-                                          (tea-number 6)))
-                         (tea-apply (tea-id 'y)
-                                    (list (tea-number 4))))))
-  (test-case
-   "let"
-   (check-equal? (pt '(let [(x 3) (y 4)]
-                        (+ x y)))
-                 (tea-let (list (tea-id 'x) (tea-id 'y))
-                          (list (tea-number 3) (tea-number 4))
-                          (list (tea-apply (tea-id '+)
-                                           (list (tea-id 'x)
-                                                 (tea-id 'y)))))))
-  (test-case
-   "quoted lists"
-   (check-equal? (pt ''(1 2 (3 4 (5 6) a b) "c" "d"))
-                 (tea-list (list (tea-number 1)
-                                 (tea-number 2)
-                                 (tea-list (list (tea-number 3)
-                                                 (tea-number 4)
-                                                 (tea-list (list
-                                                            (tea-number 5)
-                                                            (tea-number 6)))
-                                                 (tea-symbol 'a)
-                                                 (tea-symbol 'b)))
-                                 (tea-string "c")
-                                 (tea-string "d")))))))
+  (test-suite
+   "tests for the parser"
+   (test-case
+    "define statements"
+    (check-equal? (pt '(define foo 3))
+                  (tea-define (tea-id 'foo) (tea-number 3)))
+    (check-equal? (pt '(define (foo bar baz) (+ bar baz)))
+                  (tea-pdefine (tea-id 'foo)
+                               (list (tea-id 'bar)
+                                     (tea-id 'baz))
+                               (list (tea-apply (tea-id '+)
+                                                (list (tea-id 'bar)
+                                                      (tea-id 'baz)))))))
+   (test-case
+    "self-evals"
+    (check-equal? (pt '3)
+                  (tea-number 3))
+    (check-equal? (pt '"foo")
+                  (tea-string "foo"))
+    (check-equal? (pt 'foo)
+                  (tea-id 'foo))
+    (check-equal? (pt ''foo)
+                  (tea-symbol 'foo)))
+   (test-case
+    "apply"
+    (check-equal? (pt '(+ 1 foo))
+                  (tea-apply (tea-id '+)
+                             (list (tea-number 1)
+                                   (tea-id 'foo)))))
+   (test-case
+    "lambda"
+    (check-equal? (pt '(lambda (x y z) (- x y z)))
+                  (tea-lambda (list (tea-id 'x)
+                                    (tea-id 'y)
+                                    (tea-id 'z))
+                              (list (tea-apply (tea-id '-)
+                                               (list (tea-id 'x)
+                                                     (tea-id 'y)
+                                                     (tea-id 'z)))))))
+   (test-case
+    "if"
+    (check-equal? (pt '(if x (+ 3 6) (y 4)))
+                  (tea-if (tea-id 'x)
+                          (tea-apply (tea-id '+)
+                                     (list (tea-number 3)
+                                           (tea-number 6)))
+                          (tea-apply (tea-id 'y)
+                                     (list (tea-number 4))))))
+   (test-case
+    "let"
+    (check-equal? (pt '(let [(x 3) (y 4)]
+                         (+ x y)))
+                  (tea-let (list (tea-id 'x) (tea-id 'y))
+                           (list (tea-number 3) (tea-number 4))
+                           (list (tea-apply (tea-id '+)
+                                            (list (tea-id 'x)
+                                                  (tea-id 'y)))))))
+   (test-case
+    "quoted lists"
+    (check-equal? (pt ''(1 2 (3 4 (5 6) a b) "c" "d"))
+                  (tea-list (list (tea-number 1)
+                                  (tea-number 2)
+                                  (tea-list (list (tea-number 3)
+                                                  (tea-number 4)
+                                                  (tea-list (list
+                                                             (tea-number 5)
+                                                             (tea-number 6)))
+                                                  (tea-symbol 'a)
+                                                  (tea-symbol 'b)))
+                                  (tea-string "c")
+                                  (tea-string "d")))))))
 
 (require rackunit/text-ui)
 
