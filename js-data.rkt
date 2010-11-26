1f5fc5b765975e5d7777fb3f4a1014a0af66fa44
Daniel King <danking@ccs.neu.edu>
2010-11-25 16:10:43 -0800

    created `match-js' in the same spirit as `match-tea' and used it to reimplement js-expansions.rkt

--- js-data.rkt
+++ js-data.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(provide (except-out (all-defined-out) tstruct))
+(provide (except-out (all-defined-out) tstruct js-dispatcher))
 
 (define-syntax tstruct
   (syntax-rules ()
@@ -53,3 +53,54 @@
     (tstruct jlambda jexp (args body))
   ;; (jreturn JExp)
   (tstruct jreturn jstatement (exp))
+
+;; matches value according to the specified rules, just like match, but if
+;; one of the js structs is left unspecified it will default to recursively
+;; rebuild itself transforming children as specified by exp-proc and id-proc
+(define-syntax match-js
+  (syntax-rules (match-js)
+    ((match-js value exp-proc id-proc
+       (match-exp result-exp) ...)
+     (match value
+       (match-exp result-exp) ...
+       (_ (js-dispatcher value exp-proc id-proc))))))
+
+;; js-dispatcher : JStatement [JExp -> JExp] [Symbol -> Symbol]
+(define (js-dispatcher jstatement exp-proc id-proc)
+  (define (dispatch-jprim jprim)
+    (match jprim
+      [(jarray v) (jarray (exp-proc v))]
+      [_ jprim]))
+  (define (dispatch-jop jop)
+    (match jop
+      [(jprimop op args) (jprimop op (map exp-proc args))]
+      [(jbracket object property) (jbracket (exp-proc object)
+                                            property)]
+      [(jcond c t f) (jprimop (exp-proc c)
+                              (exp-proc t)
+                              (exp-proc f))]
+      [(jcomma exps) (jcomma (map exp-proc exps))]
+      [(jin property object) (jin (exp-proc property)
+                                  (exp-proc object))]
+      [(jinstof object type) (jinstof (exp-proc object)
+                                      (exp-proc type))]
+      [(jtypeof exp) (jtypeof (exp-proc exp))]
+      [(jdot object property) (jdot (exp-proc object)
+                                    (exp-proc property))]
+      [(jnew name args) (jnew (id-proc name)
+                              (map exp-proc args))]))
+
+  (match jstatement
+    [(jvdef name exp) (jvdef (id-proc name)
+                             (exp-proc exp))]
+    [(jfdef name args body) (jfdef (id-proc name)
+                                   (map id-proc args)
+                                   (map exp-proc body))]
+    [(jprim) (dispatch-jprim jstatement)]
+    [(jid name) (id-proc jstatement)]
+    [(jop) (dispatch-jop jstatement)]
+    [(japply exp args) (japply (exp-proc exp)
+                               (map exp-proc args))]
+    [(jlambda args body) (jlambda (map id-proc args)
+                                  (map exp-proc body))]
+    [(jreturn exp) (jreturn (exp-proc exp))]))
--- js-expansions.rkt
+++ js-expansions.rkt
@@ -3,63 +3,13 @@
 (provide expand-js)
 
 (define (expand-js jstatement)
-  (match jstatement
-    [(jvdef name exp) (jvdef (expand-jid name)
-                             (expand-js exp))]
-    [(jfdef name args body) (jfdef (expand-jid name)
-                                   (expand-jids args)
-                                   (expand-jss body))]
-    [(jprim) (expand-jprim jstatement)]
-    [(jid name) (expand-jid& name)]
-    [(jop) (expand-jop jstatement)]
-    [(japply exp args) (expand-japply& exp args)]
-    [(jlambda args body) (jlambda (expand-jids args)
-                                  (expand-jss body))]
-    [(jreturn exp) (jreturn (expand-js exp))]))
+  (match-js jstatement expand-js expand-jid
+    [(japply exp args) (expand-japply exp args)]))
 
 (define (expand-jss jstatements)
   (map expand-js jstatements))
 
-(define (expand-jprim jprim)
-  (match jprim
-    [(jarray v) (jarray (expand-jss v))]
-    [_ jprim]))
-
-(define (expand-jop jop)
-  (match jop
-    [(jprimop op args) (jprimop op (expand-jss args))]
-    [(jbracket object property) (jbracket (expand-js object)
-                                          (expand-jprim property))]
-    [(jcond c t f) (jprimop (expand-js c)
-                            (expand-js t)
-                            (expand-js f))]
-    [(jcomma exps) (jcomma (expand-jss exps))]
-    [(jin property object) (jin (expand-js property)
-                                (expand-js object))]
-    [(jinstof object type) (jinstof (expand-js object)
-                                    (expand-js type))]
-    [(jtypeof exp) (jtypeof (expand-js exp))]
-    [(jdot object property) (jdot (expand-js object)
-                                  (expand-js property))]
-    [(jnew name args) (jnew (expand-jid name)
-                            (expand-jss args))]))
-(define (expand-jids jids)
-  (map expand-jid jids))
-
-(define (expand-jid my-jid)
-  (match my-jid
-    [(jid name) (expand-jid& name)]
-    [_ (error 'expand-js "expected a jid got ~a" my-jid)]))
-
-(define (expand-jid& name)
-  (jid name))
-
-(define (expand-japply my-japply)
-  (match my-japply
-    [(japply op args) (expand-japply& op args)]
-    [_ (error 'expand-js "expected a japply got ~a" my-japply)]))
-
-(define (expand-japply& op args)
+(define (expand-japply op args)
   (match op
     [(jid v) (match v
                [(or '! '++_ '--_ '_++ '_--)
