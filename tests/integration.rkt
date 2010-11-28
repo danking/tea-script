0952723f5ffee2561053292eb44cc8fa840210d3
Daniel King <danking@ccs.neu.edu>
2010-11-28 14:27:36 -0800

    tests/integration.rkt now depends on main.rkt instead of multiple files. I also added support for programs (lists of expressions)

--- tests/integration.rkt
+++ tests/integration.rkt
@@ -1,38 +1,60 @@
 #lang racket
 (require rackunit
-         "../parser.rkt"
-         "../tea-to-js.rkt"
-         "../js-to-text.rkt")
+         "../main.rkt")
 
 (define sa string-append)
-(define (translate sexp)
-  (jstatement->text (tea-defexp->js (parse-tea-defexp sexp))))
-
 
 (define integration-ts
   (test-suite
    "integration tests"
-   (test-case
-    "let"
-    (check-equal? (translate '(let ([x 3] [y "foo"])
-                                (foo x y)))
-                  (sa "(function (x, y) {\n"
-                      "  return foo(x, y);\n"
-                      "})(3, \"foo\");\n")))
-   (test-case
-    "if"
-    (check-equal? (translate '(if (predicate foo bar)
-                                  (branch1 foo bar)
-                                  (branch2 foo bar)))
-                  (sa "(predicate(foo, bar) ? branch1(foo, bar) : "
-                      "branch2(foo, bar));\n")))
-   (test-case
-    "function definition"
-    (check-equal? (translate '(define (f x)
-                                (if (predicate x) 3 (f (sub1 x)))))
-                  (sa "function f (x) {\n"
-                      "  return (predicate(x) ? 3 : f(sub1(x)));\n"
-                      "};\n")))))
+   (test-suite
+    "statement integration tests"
+    (test-case
+     "let"
+     (check-equal? (tea->js '(let ([x 3] [y "foo"])
+                               (foo x y)))
+                   (sa "(function (x0, y0) {\n"
+                       "  return foo0(x0, y0);\n"
+                       "})(3, \"foo\");\n")))
+    (test-case
+     "if"
+     (check-equal? (tea->js '(if (predicate foo bar)
+                                 (branch1 foo bar)
+                                 (branch2 foo bar)))
+                   (sa "(predicate0(foo0, bar0) ? branch10(foo0, bar0) : "
+                       "branch20(foo0, bar0));\n")))
+    (test-case
+     "raise"
+     (check-equal? (tea->js '(raise "OH SHIT!"))
+                   "throw \"OH SHIT!\";\n"))
+    (test-case
+     "function definition"
+     (check-equal? (tea->js '(define (f x)
+                               (if (predicate x) 3 (f (sub1 x)))))
+                   (sa "function f0 (x0) {\n"
+                       "  return (predicate0(x0) ? 3 : f0(sub10(x0)));\n"
+                       "};\n"))))
+   (test-suite
+    "program integration tests"
+    (test-case
+     "define and use"
+     (check-equal? (tea-program->js-program '((define foo 3)
+                                              (+ foo 6)))
+                   (sa "var foo0 = 3;\n"
+                       "a0(foo0, 6);\n")))
+    (test-case
+     "factorial and use"
+     (check-equal? (tea-program->js-program '((define (factorial x)
+                                                (if (< x 2)
+                                                    1
+                                                    (* x
+                                                       (factorial (sub1 x)))))
+                                              (factorial 5)))
+                   (sa "function factorial0 (x0) {\n"
+                       "  return (l0(x0, 2) ? 1 : "
+                       "m0(x0, factorial0(sub10(x0))));\n"
+                       "};\n"
+                       "factorial0(5);\n"))))))
 
 (require rackunit/text-ui)
 
