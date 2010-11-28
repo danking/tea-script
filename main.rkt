39213d380ca4005354a502989b9df428f35eec2d
Daniel King <danking@ccs.neu.edu>
2010-11-28 14:26:41 -0800

    adding program (lists of expressions) support to main.rkt

--- main.rkt
+++ main.rkt
@@ -1,11 +1,20 @@
 #lang racket
 (require "parser.rkt"
+         "sanitize-ids.rkt"
          "tea-to-js.rkt"
          "js-expansions.rkt"
-         "js-to-text.rkt"
-         "sanitize-ids.rkt")
-(provide tea->js)
+         "js-to-text.rkt")
+(provide tea->js tea-program->js-program)
 
+;; tea->js : SExp
 (define (tea->js sexp)
   (jstatement->text
    (expand-js (tea-defexp->js (sanitize-exp (parse-tea-defexp sexp))))))
+
+;; tea-program->js-program : [ListOf SExp]
+(define (tea-program->js-program sexps)
+  (foldr (lambda (statement program)
+           (string-append (tea->js statement)
+                          program))
+         ""
+         sexps))
