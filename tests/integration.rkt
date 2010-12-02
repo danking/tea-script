1cd836165ec6c0ab3bb090fa147f6ebb0c38402a
Daniel King <danking@ccs.neu.edu>
2010-12-01 21:40:16 -0800

    yay, identifiers work properly now, restored proper integration unit tests

--- tests/integration.rkt
+++ tests/integration.rkt
@@ -13,16 +13,16 @@
      "let"
      (check-equal? (tea->js '(let ([x 3] [y "foo"])
                                (foo x y)))
-                   (sa "(function (x0, y0) {\n"
-                       "  return foo0(x0, y0);\n"
+                   (sa "(function (x, y) {\n"
+                       "  return foo(x, y);\n"
                        "})(3, \"foo\");\n")))
     (test-case
      "if"
      (check-equal? (tea->js '(if (predicate foo bar)
                                  (branch1 foo bar)
                                  (branch2 foo bar)))
-                   (sa "(predicate0(foo0, bar0) ? branch10(foo0, bar0) : "
-                       "branch20(foo0, bar0));\n")))
+                   (sa "(predicate(foo, bar) ? branch1(foo, bar) : "
+                       "branch2(foo, bar));\n")))
     (test-case
      "raise"
      (check-equal? (tea->js '(raise "OH SHIT!"))
@@ -31,8 +31,8 @@
      "function definition"
      (check-equal? (tea->js '(define (f x)
                                (if (predicate x) 3 (f (sub1 x)))))
-                   (sa "function f0 (x0) {\n"
-                       "  return (predicate0(x0) ? 3 : f0(sub10(x0)));\n"
+                   (sa "function f (x) {\n"
+                       "  return (predicate(x) ? 3 : f(sub1(x)));\n"
                        "};\n"))))
    (test-suite
     "program integration tests"
@@ -40,8 +40,8 @@
      "define and use"
      (check-equal? (tea-program->js-program '((define foo 3)
                                               (+ foo 6)))
-                   (sa "var foo0 = 3;\n"
-                       "a0(foo0, 6);\n")))
+                   (sa "var foo = 3;\n"
+                       "a0(foo, 6);\n")))
     (test-case
      "factorial and use"
      (check-equal? (tea-program->js-program '((define (factorial x)
@@ -50,11 +50,11 @@
                                                     (* x
                                                        (factorial (sub1 x)))))
                                               (factorial 5)))
-                   (sa "function factorial0 (x0) {\n"
-                       "  return (l0(x0, 2) ? 1 : "
-                       "m0(x0, factorial0(sub10(x0))));\n"
+                   (sa "function factorial (x) {\n"
+                       "  return (l0(x, 2) ? 1 : "
+                       "m0(x, factorial(sub1(x))));\n"
                        "};\n"
-                       "factorial0(5);\n"))))))
+                       "factorial(5);\n"))))))
 
 (require rackunit/text-ui)
 
