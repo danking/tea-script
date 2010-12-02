b8e6674b5eabab7dc10d10d007a645463f171ece
Daniel King <danking@ccs.neu.edu>
2010-12-01 21:27:01 -0800

    WOOT!  Properly sanitized identifiers!!

--- sanitize-ids.rkt
+++ sanitize-ids.rkt
@@ -1,28 +1,53 @@
 #lang racket
 (require "tea-data.rkt"
          "make-uid.rkt")
-(provide sanitize-exp)
+(provide sanitize-ids)
 
-(define id-map '())
+(define (sanitize-ids exp)
+  (let ([id-map (sanitize-symbols (gather-ids exp))])
+    (sanitize-ids/id-map exp id-map)))
 
+(define (sanitize-ids/id-map exp id-map)
+  (match-tea exp
+             (lambda (exp) (sanitize-ids/id-map exp id-map))
+             (lambda (id)
+               (tea-id
+                (hash-ref id-map
+                          (tea-id-value id)
+                          (lambda ()
+                            (error 'sanitize-ids
+                                   (string-append
+                                    "somehow we didn't record an id in "
+                                    "the gather-ids phase; end users "
+                                    "should never see this -- ~a ~a")
+                                   id
+                                   id-map)))))))
 
-(define (sanitize-exp exp)
-  (set! id-map '())
-  (sanitize-exp* exp))
+;; gather-ids : Tea-Expression -> [ListOf Symbol]
+(define (gather-ids exp)
+  (set->list (gather-ids/set exp)))
 
-(define (sanitize-exp* exp)
-  (match-tea exp sanitize-exp* sanitize-tea-id))
+(define (gather-ids/set exp)
+  (tea-accumulator exp
+                   (lambda (list-of-sets)
+                     (foldl (lambda (subset superset)
+                              (set-union subset superset))
+                            (set)
+                            list-of-sets))
+                   gather-ids/set
+                   (lambda (id) (set (tea-id-value id)))))
 
-;; add-id-mapping : Symbol Symbol [ListOf Symbol]
-(define (add-id-mapping old new)
-  (cons (cons old new) id-map))
+;; set->list : [SetOf X] -> [ListOf X]
+(define (set->list set)
+  (set-map set (lambda (x) x)))
 
-;; sanitize-tea-id : Tea-Id -> Tea-Id
-(define (sanitize-tea-id tid)
-  (tea-id (sanitize-symbol (tea-id-value tid))))
-
-;; sanitize-id : Symbol -> Symbol
-(define (sanitize-symbol old-id)
-  (let ([new-id (make-id-unique old-id id-map)])
-    (set! id-map (add-id-mapping old-id new-id))
-    new-id))
+;; sanitize-symbols : [ListOf Symbol] -> [Hasheq Symbol -> Symbol]
+(define (sanitize-symbols ids)
+  (let loop ([ids-to-make-unique ids]
+             [id-map (hasheq)])
+    (if (empty? ids-to-make-unique)
+        id-map
+        (let* ([old-id (first ids-to-make-unique)]
+               [new-id (make-symbol-unique old-id (hash-values id-map))])
+          (loop (rest ids-to-make-unique)
+                (hash-set id-map old-id new-id))))))
--- tests/sanitize-ids.rkt
+++ tests/sanitize-ids.rkt
@@ -4,7 +4,7 @@
          "../sanitize-ids.rkt")
 
 (define p parse-tea-defexp)
-(define (sp sexp) (sanitize-exp (p sexp)))
+(define (sp sexp) (sanitize-ids (p sexp)))
 
 (define sanitize-ids-ts
   (test-suite
@@ -12,17 +12,17 @@
    (test-case
     "define statements"
     (check-equal? (sp '(define x 3))
-                  (p '(define x0 3)))
+                  (p '(define x 3)))
     (check-equal? (sp '(define foo-bar_ 3))
                   (p '(define foo_bar_0 3)))
     (check-equal? (sp '(define (foo-bar_ x)
                          (if (empty? x)
                              'foo-bar_
                              (foo-bar_ (rest x)))))
-                  (p '(define (foo_bar_0 x0)
-                        (if (emptyp0 x0)
+                  (p '(define (foo_bar_0 x)
+                        (if (emptyp0 x)
                             'foo-bar_
-                            (foo_bar_0 (rest0 x0)))))))
+                            (foo_bar_0 (rest x)))))))
    (test-case
     "self-evals"
     (check-equal? (sp ''x)
@@ -34,12 +34,12 @@
    (test-case
     "lambda"
     (check-equal? (sp '(lambda (x) x))
-                  (p '(lambda (x0) x0)))
+                  (p '(lambda (x) x)))
     (check-equal? (sp '(lambda (foo-bar) foo-bar))
                   (p '(lambda (foo_bar0) foo_bar0)))
     (check-equal? (sp '(lambda (foo-bar foo_bar)
                          (+ foo-bar foo-bar)))
-                  (p '(lambda (foo_bar0 foo_bar1)
+                  (p '(lambda (foo_bar0 foo_bar)
                         (a0 foo_bar0 foo_bar0)))))
    (test-case
     "let"
@@ -49,20 +49,22 @@
                         foo_bar0)))
     (check-equal? (sp '(let ([foo_bar 4] [foo~bar 2])
                          (+ (* 10 foo_bar) foo~bar)))
-                  (p '(let ([foo_bar0 4] [foo_bar1 2])
-                        (a0 (m0 10 foo_bar0) foo_bar1)))))
+                  (p '(let ([foo_bar 4] [foo_bar0 2])
+                        (a0 (m0 10 foo_bar) foo_bar0)))))
    (test-case
     "nested lets and lambdas"
+    ;; this occasionally fails because of order is lost in the set of symbols,
+    ;; but this does not cause identifier collisions
     (check-equal? (sp '(let ([foo-bar 3])
                          (let ([foo_bar 5])
                            ((lambda (x foo~bar)
                               (foo.bar foo_bar foo-bar foo~bar x))))))
                   (p '(let ([foo_bar0 3])
-                        (let ([foo_bar1 5])
-                          ((lambda (x0 foo_bar2)
-                             (foo_bar3 foo_bar1 foo_bar0 foo_bar2 x0))))))))
+                        (let ([foo_bar 5])
+                          ((lambda (x foo_bar2)
+                             (foo_bar1 foo_bar foo_bar0 foo_bar2 x))))))))
    (test-case
-    "stary-eyed fantasy"
+    "stary-eyed fantasy -- SUCCESS!"
     (check-equal? (sp '(let ([foo-bar 3])
                          (let ([foo_bar 5])
                            (let ([foo~bar 7])
