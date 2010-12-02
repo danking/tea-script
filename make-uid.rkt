44ce7e27df5aa8c31051608699e6c17f84edd67f
Daniel King <danking@ccs.neu.edu>
2010-12-01 21:25:25 -0800

    stripped make-uid down to its most basic parts

--- make-uid.rkt
+++ make-uid.rkt
@@ -1,43 +1,19 @@
 #lang racket
-(provide sanitize-env
-         make-id-unique)
+(provide make-symbol-unique)
 
-;; a Frame is a non-empty [ListOf Symbol]
-
-;; an [AList X X] is a [ListOf (cons X X)]
-
-(define (sanitize-env env)
-  (accum (lambda (old-frame id-map)
-           (let ([new-frame (sanitize-frame old-frame id-map)])
-             (values new-frame
-                     (append (map cons old-frame new-frame) id-map))))
-         '()
-         env))
-
-;; make-uid/frame : Frame [AList Symbol Symbol] -> Frame
-(define (sanitize-frame frame id-map)
-  (accum (lambda (old-id id-map)
-           (let ([new-id (make-id-unique old-id id-map)])
-             (values new-id
-                     (cons (cons old-id new-id) id-map))))
-         id-map
-         frame))
-
-(define (make-id-unique id id-map)
-  (if #t ; (bad-id? id)
-      (let ([id&uid (assq id id-map)])
-        (if id&uid
-            (cdr id&uid)
-            (make-id-unique* (sanitize id) 0 (map cdr id-map))))
+;; make-symbol-unique : Symbol [ListOf Symbol] -> Symbol
+(define (make-symbol-unique id used)
+  (if (bad-id? id)
+      (make-symbol-unique* (sanitize id) 0 used)
       id))
 
-;; make-id-unique* : Symbol Number [ListOf Symbol] -> Symbol
-(define (make-id-unique* id i used)
+(define (make-symbol-unique* id i used)
   (let ([next-id (append-number id i)])
     (if (memq next-id used)
-        (make-id-unique* id (add1 i) used)
+        (make-symbol-unique* id (add1 i) used)
         next-id)))
 
+;; append-number : Symbol Number -> Symbol
 (define (append-number id i)
   (string->symbol (string-append (symbol->string id)
                                  (number->string i))))
@@ -46,6 +22,16 @@
 (define (sanitize id)
   (let ([chars (string->list (symbol->string id))])
     (string->symbol (list->string (map sanitize-char chars)))))
+
+;; bad-id? : Symbol -> Boolean
+(define (bad-id? str)
+  (ormap (lambda (char) (assq char char-map))
+         (string->list (symbol->string str))))
+
+;; sanitize-char : Character -> Character
+(define (sanitize-char char)
+  (let ([mapping (assq char char-map)])
+    (if mapping (cdr mapping) char)))
 
 ;; try to avoid a many-to-one relationship in char-map
 (define char-map
@@ -66,34 +52,3 @@
     [#\> . #\g]
     [#\: . #\_]
     [#\? . #\p]))
-
-;; bad-id? : Symbol -> Boolean
-(define (bad-id? str)
-  (ormap (lambda (char) (assq char char-map))
-         (string->list (symbol->string str))))
-
-(define (sanitize-char char)
-  (let ([mapping (assq char char-map)])
-    (if mapping (cdr mapping) char)))
-
-;; (define (accum map-proc acc-proc acc ls)
-;;   (let loop ([acc acc] [ls ls])
-;;     (if (null? ls)
-;;       '()
-;;       (cons (map-proc (first ls) acc)
-;;             (loop (acc-proc acc (first ls)) (rest ls))))))
-
-;; (define (accum proc acc ls)
-;;   (let loop ([acc acc] [ls ls])
-;;     (if (empty? ls)
-;;       '()
-;;       (let-values ([(next-element next-acc) (proc (first ls) acc)])
-;;         (cons next-element (loop next-acc (rest ls)))))))
-
-(define (stateful-proc state proc)
-  (lambda (x)
-    (let-values ([(result next-state) (proc x state)])
-      (set! state next-state)
-      result)))
-(define (accum proc acc ls)
-  (map (stateful-proc acc proc) ls))
--- tests/make-uid.rkt
+++ tests/make-uid.rkt
@@ -6,26 +6,19 @@
   (test-suite
    "tests for make-uid.rkt"
    (test-case
-    "one frame"
-    (check-equal? (sanitize-env '((foo-bar_ foo-bar-)))
-                  '((foo_bar_0 foo_bar_1))))
+    "clean symbols"
+    (check-equal? (make-symbol-unique 'foo (list 'foo 'bar))
+                  'foo)
+    (check-equal? (make-symbol-unique 'bar (list 'foo 'bar))
+                  'bar)
+    (check-equal? (make-symbol-unique 'baz (list 'foo 'bar))
+                  'baz))
    (test-case
-    "two frames"
-    (check-equal? (sanitize-env '((foo-bar_) (foo-bar-)))
-                  '((foo_bar_0) (foo_bar_1)))
-    (check-equal? (sanitize-env '((foo-bar_) (foo-bar_)))
-                  '((foo_bar_0) (foo_bar_0))))
-   ;; this test case depends on not changing "clean" ids, which currently
-   ;; isn't supported because I haven't built the infrastructure
-   ;; in sanitize-ids to properly work like this
-   (test-case
-    "three frames and clean ids"
-    (check-equal? (sanitize-env '((foo_bar_)
-                                  (foo-bar_)
-                                  (foo_bar- foo_bar_ foo-bar_)))
-                  '((foo_bar_)
-                    (foo_bar_0)
-                    (foo_bar_1 foo_bar_ foo_bar_0))))))
+    "dirty symbols"
+    (check-equal? (make-symbol-unique 'foo- (list 'foo_))
+                  'foo_0)
+    (check-equal? (make-symbol-unique 'foo- (list 'foo_ 'foo_0))
+                  'foo_1))))
 
 (require rackunit/text-ui)
 
