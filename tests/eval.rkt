1f6020115624f39eef5dae0f428bbe90d9d006e9
Daniel King <danking@ccs.neu.edu>
2010-12-15 22:39:09 -0800

    fixed and updated eval.rkt tests

--- tests/eval.rkt
+++ tests/eval.rkt
@@ -3,26 +3,41 @@
          "../eval.rkt")
 (provide eval-ts)
 
-(define-syntax check-runtime
-  (syntax-rules ()
-   [(_ sexp expected) (let [(output (tea-eval sexp))]
-                        (check-equal? (first output)
-                                      expected
-                                      (second output)))]))
-
 ;; symbols
 (define eval-ts
  (test-suite
   "runtime tests"
   (test-case
    "non-js friendly ids"
-   (check-runtime '(symbol=? foo-bar_ foo_bar-)
+   (check-equal? (tea-eval '(symbol=? 'foo-bar_ 'foo_bar-))
                   "false"))            ; depends on gensym
   (test-case
+   "addition"
+   (check-equal? (tea-eval '(+ 1 2 3))
+                 "6"))
+  (test-case
+   "lists"
+   (check-equal? (tea-eval ''(1 2 3))
+                 "1,2,3")
+   (check-equal? (tea-eval ''(1 2 ("a" () "b") "foo"))
+                 (string-append "1,2,a,,b,foo")))
+  (test-case
+   "map"
+   (check-equal? (tea-eval '(map (lambda (x) (* x x)) '(1 2 3 4 5 6)))
+                 "1,4,9,16,25,36"))
+  (test-case
+   "foldr"
+   (check-equal? (tea-eval '(foldr cons '() '(1 2 3 4 5)))
+                 "1,2,3,4,5"))
+  (test-case
+   "foldl"
+   (check-equal? (tea-eval '(foldl cons '() '(1 2 3 4 5)))
+                 "5,4,3,2,1"))
+  (test-case
    "let"
-   (check-runtime '(let ([x 3] [y 5])
-                        (+ x y 2))
-                  "10"))))
+   (check-equal? (tea-eval '(let ([x 3] [y 5])
+                              (+ x y 2)))
+                 "10"))))
 
 (require rackunit/text-ui)
 
