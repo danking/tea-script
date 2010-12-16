dcba5ef7a04481e678c0671fd5eb8e0eae0157fa
Daniel King <danking@ccs.neu.edu>
2010-12-15 22:49:20 -0800

    *EXPERIMENTAL* added a repl which `works' but doesn't accumulate an environment

---
+++ repl.rkt
@@ -0,0 +1,18 @@
+#lang racket
+(require rackunit
+         "eval.rkt")
+
+;;; THIS IS EXPERIMENTAL.
+;;; IT DOES NOT ACCUMULATE AN ENVIRONMENT.
+;;; DON'T USE IT EXCEPT FOR FUN AND STUFF.
+(define (tea-eval/repl stx)
+  (let* ([sexp (cdr (syntax->datum stx))])
+    (tea-eval sexp)))
+
+(current-eval tea-eval/repl)
+(current-print (lambda (value)
+                 (unless (or (void? value)
+                             (and (string? value)
+                                  (string=? value "js> ")))
+                   (displayln value))))       ; print strings without the quotes
+(read-eval-print-loop)
