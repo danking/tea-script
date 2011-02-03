30220e15ccc67881579c8084acc74b6645625472
Daniel King <danking@ccs.neu.edu>
2011-02-02 21:04:50 -0500

    wrote some stupidly complicated macro to do a simple chore.  BUT ITS SO COOL!

--- tests/run-all.rkt
+++ tests/run-all.rkt
@@ -1,10 +1,42 @@
 #lang racket
-(require "parser.rkt")
-(require "tea-to-js.rkt")
-(require "sanitize-ids.rkt")
-(require "js-expansions.rkt")
-(require "js-to-text.rkt")
-(require "integration.rkt")
-(require "eval.rkt")
-(require "string-utilities.rkt")
+(require rackunit)
+
+(define-syntax (require/run-test-suites stx)
+  (define (test-suite-names->filenames test-suite-names)
+    (map (lambda (test-suite-name)
+           (string-append (symbol->string test-suite-name)
+                          ".rkt"))
+         test-suite-names))
+  (define (test-suite-names->test-suite-ids test-suite-names)
+    (map (lambda (test-suite-name)
+           (string->symbol (string-append (symbol->string test-suite-name)
+                                          "-ts")))
+         test-suite-names))
+
+  (syntax-case stx ()
+    [(require/run-test-suites test-suite-name ...)
+     (with-syntax
+         ([(filename ...)
+           (datum->syntax #'(test-suite-name ...)
+                          (test-suite-names->filenames
+                           (syntax->datum #'(test-suite-name ...))))]
+          [(test-suite-id ...)
+           (datum->syntax #'(test-suite-name ...)
+                          (test-suite-names->test-suite-ids
+                           (syntax->datum #'(test-suite-name ...))))])
+      #'(begin
+          (require rackunit/text-ui
+                   filename ...)
+          (run-tests (test-suite
+                      "meta-test-suite"
+                      test-suite-id ...))))]))
+
+(require/run-test-suites parser
+                         tea-to-js
+                         sanitize-ids
+                         js-expansions
+                         js-to-text
+                         integration
+                         eval
+                         string-utilities)
 (display (port->string (first (process "rhino environment.js"))))
