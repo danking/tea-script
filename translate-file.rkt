034b78c52b7467a1875464fb9a8d7c1258b4cc83
Daniel King <danking@ccs.neu.edu>
2011-04-18 17:12:09 -0400

    added the environment to translate-file.rkt

--- translate-file.rkt
+++ translate-file.rkt
@@ -1,10 +1,18 @@
 #lang racket
-(require "main.rkt")
+(require "main.rkt"
+         racket/runtime-path)
 (provide translate-file)
 
+(define-runtime-path js-environment-file "environment.js")
+
 (define (translate-file path)
-  (display-to-file (tea-program->js-program (file->sexps path))
-                   "a.out"
+  (display-to-file (string-append
+                    ;; include the environment
+                    (file->string js-environment-file)
+                    "\nEnvironmentModule(this);\n\n"
+                    ;; include our js code
+                    (tea-program->js-program (file->sexps path)))
+                   "a.js"
                    #:exists 'replace))
 
 (define (file->sexps path)
